1b6c07c5655c64fcdd0dc4e6f2f4cd358e8ef54b
bzavo <bzavoevanyy@gmail.com>
2021-01-09 02:09:17 +0500

    third

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,9 +2,13 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.util.CommandArgumentParser;
 import com.pi4j.util.Console;
 import com.pi4j.util.ConsoleColor;
+import com.pi4j.platform.Platform;
+import com.pi4j.platform.PlatformAlreadyAssignedException;
+import com.pi4j.platform.PlatformManager;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PlatformAlreadyAssignedException {
+        PlatformManager.setPlatform(Platform.ORANGEPI);
         final GpioController gpio = GpioFactory.getInstance();
         Pin pin = CommandArgumentParser.getPin(OrangePiPin.class, OrangePiPin.GPIO_00, args);
 
