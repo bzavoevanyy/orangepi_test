1c8ec8a3dda87d3231a308febd0a9454c7596f25
bzavo <bzavoevanyy@gmail.com>
2021-01-09 02:04:21 +0500

    second

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,17 @@
 import com.pi4j.io.gpio.*;
+import com.pi4j.util.CommandArgumentParser;
+import com.pi4j.util.Console;
+import com.pi4j.util.ConsoleColor;
 
 public class App {
     public static void main(String[] args) {
         final GpioController gpio = GpioFactory.getInstance();
-        GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(OrangePiPin.GPIO_00);
+        Pin pin = CommandArgumentParser.getPin(OrangePiPin.class, OrangePiPin.GPIO_00, args);
+
+        GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(pin);
         PinState myButtonState = myButton.getState();
-        System.out.println(myButtonState.toString());
+        Console console = new Console();
+        console.println(pin.getSupportedPinModes());
+//        System.out.println(myButtonState.toString());
     }
 }
