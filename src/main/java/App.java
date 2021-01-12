a03ce943ac20cea27bbae1db246a1d7e46347442
bzavo <bzavoevanyy@gmail.com>
2021-01-12 19:45:10 +0500

    four

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.pi4j.platform.Platform;
 import com.pi4j.platform.PlatformAlreadyAssignedException;
 import com.pi4j.platform.PlatformManager;
@@ -92,9 +94,22 @@ public class App {
                 ConsoleColor.RED,          // negative conditional color
                 input.getState()));
         console.emptyLine();
+        final GpioPinDigitalOutput output = gpio.provisionDigitalOutputPin(OrangePiPin.GPIO_01, "output", PinState.HIGH);
+        output.setShutdownOptions(true);
 
+        input.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+            }
+
+        });
+        while (true) {
+            Thread.sleep(500);
+        }
         // stop all GPIO activity/threads by shutting down the GPIO controller
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        gpio.shutdown();
+        //gpio.shutdown();
     }
 }
